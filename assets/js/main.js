990ae2b673772d489c78d2deea5a326a8c81291f
Malabika Sen <malabika1310@gmail.com>
2020-12-25 03:36:50 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ function darkModeToggle(obj) {
     //alert("Yes checked");
     
     //element.classList.toggle("dark-mode");//when checked
-    document.body.style.background = "#95a5a6";
+    document.body.style.background = "#2c3e50";
     element.style.backgroundColor = "#34495e";
   }else{
     document.body.style.background = "#ecf0f1"; //when not checked
